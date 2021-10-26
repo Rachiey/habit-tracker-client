b1e6f388b56cf846504f617dd5bb97084cf09e90
jpxllington <jjdp99@gmail.com>
2021-10-26 16:45:28 +0100

    add back button, add circular progress bar (not fully functional)

---
+++ static/habits.js
@@ -0,0 +1,13 @@
+const progress = document.querySelector('.circular-progress-1')
+
+const updateProgress = value => {
+  progress.style.setProperty('--percentage', `${value * 3.6}deg`)
+  progress.innerText = `${value}%`
+}
+
+updateProgress(0)
+
+document.querySelector('input')
+  .addEventListener('input', e => {
+    updateProgress(e.currentTarget.value)
+  })
